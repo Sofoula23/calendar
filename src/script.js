248fd7a5dfe9f1b2cf7237f86b7c84f202d23451
Sofia <sofimantas@gmail.com>
2021-01-04 21:43:02 -0500

    input info in local storage and save users input

--- src/script.js
+++ src/script.js
@@ -5,18 +5,13 @@ var currentHour = moment().format("h:mm:ss a");
 
 //Save the input information in the local storage
 $(document).ready(function(){
+    
     $(".saveBtn").on("click", function(){
-        var value = $("textarea.event-text").val();
-        var time = $(this).parent().attr("id")
-        localStorage.setItem(time, value)
-
+        var value = $(this).siblings(".event-text").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, value);
+    
     })
-    // event listener for save buttons to add to local storage
-    $(".saveBtn").on("click", function () {
-        var click = $(this).attr("data-value");
-        var eventInput = $(click).val();
-        localStorage.setItem(click, eventInput);
-    });
     //Determine color code for the past, the present and the future. 
     function determineHour(){
         var currentTime = moment().hours();
@@ -38,4 +33,16 @@ $(document).ready(function(){
         })
     }
     determineHour()
+     //Place the current date 
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    //get saved items by user from Local Storage
+    $("#time-9 .event-text").val(localStorage.getItem("time-9"))
+    $("#time-10 .event-text").val(localStorage.getItem("time-10"))
+    $("#time-11 .event-text").val(localStorage.getItem("time-11"))
+    $("#time-12 .event-text").val(localStorage.getItem("time-12"))
+    $("#time-13 .event-text").val(localStorage.getItem("time-13"))
+    $("#time-14 .event-text").val(localStorage.getItem("time-14"))
+    $("#time-15 .event-text").val(localStorage.getItem("time-15"))
+    $("#time-16 .event-text").val(localStorage.getItem("time-16"))
+    $("#time-17 .event-text").val(localStorage.getItem("time-17"))
 })
